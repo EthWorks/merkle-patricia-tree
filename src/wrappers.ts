dce1d3bef9d2eb5a836b1f8f7e8e0a9ac8fb3299
Michał Sieczkowski <michal@ethworks.io>
2020-04-17 16:41:33 +0200

    Wrap del and getRaw functions

--- src/wrappers.ts
+++ src/wrappers.ts
@@ -1,7 +1,10 @@
 import { CheckpointTrie } from './checkpointTrie'
 import { SecureTrie } from './secure-original'
 
-function wrapEmptyPromise(promise: Promise<void>, cb?: (err: any) => void) {
+type EmptyCallback = (err: any) => void
+type BufferCallback = (err: any, value: Buffer | null) => void
+
+function wrapEmptyPromise(promise: Promise<void>, cb?: EmptyCallback): Promise<void> {
   return promise.then(
     () => {
       cb?.(null)
@@ -16,30 +19,56 @@ function wrapEmptyPromise(promise: Promise<void>, cb?: (err: any) => void) {
   )
 }
 
+function wrapBufferPromise(
+  promise: Promise<Buffer | null>,
+  cb?: BufferCallback,
+): Promise<Buffer | null> {
+  return promise.then(
+    (value) => {
+      cb?.(null, value)
+      return value
+    },
+    (err) => {
+      if (cb) {
+        cb(err, null)
+        return undefined as any
+      } else {
+        throw err
+      }
+    },
+  )
+}
+
 class WrappedCheckpointTrie extends CheckpointTrie {
-  async get(key: Buffer, cb?: (err: any, value: Buffer | null) => void): Promise<Buffer | null> {
-    return super.get(key).then(
-      (value) => {
-        cb?.(null, value)
-        return value
-      },
-      (err) => {
-        if (cb) {
-          cb(err, null)
-          return undefined as any
-        } else {
-          throw err
-        }
-      },
-    )
+  async get(key: Buffer, cb?: BufferCallback): Promise<Buffer | null> {
+    return wrapBufferPromise(super.get(key), cb)
   }
 
-  async put(key: Buffer, value: Buffer, cb?: (err: any) => void): Promise<void> {
+  async put(key: Buffer, value: Buffer, cb?: EmptyCallback): Promise<void> {
     return wrapEmptyPromise(super.put(key, value), cb)
   }
 
-  async commit(cb?: (err: any) => void): Promise<void> {
+  async commit(cb?: EmptyCallback): Promise<void> {
     return wrapEmptyPromise(super.commit(), cb)
+  }
+
+  async del(key: Buffer, cb?: EmptyCallback): Promise<void> {
+    return wrapEmptyPromise(super.del(key), cb)
+  }
+
+  async getRaw(key: Buffer, cb?: BufferCallback): Promise<Buffer | null> {
+    return wrapBufferPromise(this.db.get(key), cb)
+  }
+
+  copy(includeCheckpoints: boolean = true): WrappedCheckpointTrie {
+    const db = this._mainDB.copy()
+    const trie = new WrappedCheckpointTrie(db._leveldb, this.root)
+    if (includeCheckpoints && this.isCheckpoint) {
+      trie._checkpoints = this._checkpoints.slice()
+      trie._scratch = this._scratch!.copy()
+      trie.db = trie._scratch
+    }
+    return trie
   }
 }
 
