391d4bc7ddabff63c077704d1fedb93d337c3c93
Michał Sieczkowski <mikee2509@gmail.com>
2020-04-17 19:50:04 +0200

    Make most of public facing API backwards compatible (#3)
    * Add yarn.lock
    
    Co-authored-by: Michał Sieczkowski <mikee2509@gmail.com>
    
    * Add wrapped tries and correct exports
    
    Co-authored-by: Michał Sieczkowski <mikee2509@gmail.com>
    
    * Improve wrapping
    
    * Change get and commit wrappers order
    
    * Wrap put method
    
    * Extract wrapEmptyPromise function
    
    * Wrap del and getRaw functions
    
    * Generalise Callback type and warp function
    
    * Make wrapper params types match the ones from old API
    
    * Add putRaw function wrapper
    
    * Fix lint
    
    * Wrap checkRoot function
    
    * Move commit wrapper down
    
    * Wrap revert function
    
    * Wrap methods of SecureTrie as well
    
    * Allow any strings to be keys in WrappedSecureTrie
    
    Co-authored-by: Piotr Szlachciak <szlachciak.piotr@gmail.com>

---
+++ src/wrappers.ts
@@ -0,0 +1,152 @@
+import { CheckpointTrie } from './checkpointTrie'
+import { SecureTrie } from './secure-original'
+import { toBuffer } from 'ethereumjs-util'
+
+const ethjsUtil = require('ethjs-util')
+
+type Callback<T> = (err: any, value: T) => void
+
+function wrapEmptyPromise(promise: Promise<void>, cb?: Callback<void>): Promise<void> {
+  return promise.then(
+    () => {
+      cb?.(null)
+    },
+    (err) => {
+      if (cb) {
+        cb(err)
+      } else {
+        throw err
+      }
+    },
+  )
+}
+
+function wrapPromise<T>(promise: Promise<T | null>, cb?: Callback<T | null>): Promise<T | null> {
+  return promise.then(
+    (value) => {
+      cb?.(null, value)
+      return value
+    },
+    (err) => {
+      if (cb) {
+        cb(err, null)
+        return undefined as any
+      } else {
+        throw err
+      }
+    },
+  )
+}
+
+class WrappedCheckpointTrie extends CheckpointTrie {
+  async get(key: Buffer | string, cb?: Callback<Buffer | null>): Promise<Buffer | null> {
+    return wrapPromise(super.get(toBuffer(key)), cb)
+  }
+
+  async put(key: Buffer | string, value: Buffer | string, cb?: Callback<void>): Promise<void> {
+    let val
+    if (typeof value === 'string') {
+      if (ethjsUtil.isHexString(value)) {
+        val = toBuffer(value)
+      } else {
+        val = value as any // hack to make the tests work
+      }
+    } else {
+      val = value
+    }
+    return wrapEmptyPromise(super.put(toBuffer(key), val), cb)
+  }
+
+  async del(key: Buffer | string, cb?: Callback<void>): Promise<void> {
+    return wrapEmptyPromise(super.del(toBuffer(key)), cb)
+  }
+
+  async getRaw(key: Buffer, cb?: Callback<Buffer | null>): Promise<Buffer | null> {
+    return wrapPromise(this.db.get(key), cb)
+  }
+
+  async putRaw(key: Buffer | string, value: Buffer, cb?: Callback<void>): Promise<void> {
+    return wrapEmptyPromise(this.db.put(toBuffer(key), value), cb)
+  }
+
+  copy(includeCheckpoints: boolean = true): WrappedCheckpointTrie {
+    const db = this._mainDB.copy()
+    const trie = new WrappedCheckpointTrie(db._leveldb, this.root)
+    if (includeCheckpoints && this.isCheckpoint) {
+      trie._checkpoints = this._checkpoints.slice()
+      trie._scratch = this._scratch!.copy()
+      trie.db = trie._scratch
+    }
+    return trie
+  }
+
+  async checkRoot(root: Buffer, cb?: Callback<boolean | null>): Promise<boolean> {
+    return wrapPromise(super.checkRoot(root), cb).then((value) => !!value)
+  }
+
+  async commit(cb?: Callback<void>): Promise<void> {
+    return wrapEmptyPromise(super.commit(), cb)
+  }
+
+  async revert(cb?: Callback<void>): Promise<void> {
+    return wrapEmptyPromise(super.revert(), cb)
+  }
+}
+
+class WrappedSecureTrie extends SecureTrie {
+  async get(key: Buffer | string, cb?: Callback<Buffer | null>): Promise<Buffer | null> {
+    return wrapPromise(super.get(key as Buffer), cb)
+  }
+
+  async put(key: Buffer | string, value: Buffer | string, cb?: Callback<void>): Promise<void> {
+    let val
+    if (typeof value === 'string') {
+      if (ethjsUtil.isHexString(value)) {
+        val = toBuffer(value)
+      } else {
+        val = value as any // hack to make the tests work
+      }
+    } else {
+      val = value
+    }
+    return wrapEmptyPromise(super.put(key as Buffer, val), cb)
+  }
+
+  async del(key: Buffer | string, cb?: Callback<void>): Promise<void> {
+    return wrapEmptyPromise(super.del(key as Buffer), cb)
+  }
+
+  async getRaw(key: Buffer, cb?: Callback<Buffer | null>): Promise<Buffer | null> {
+    return wrapPromise(this.db.get(key), cb)
+  }
+
+  async putRaw(key: Buffer | string, value: Buffer, cb?: Callback<void>): Promise<void> {
+    return wrapEmptyPromise(this.db.put(toBuffer(key), value), cb)
+  }
+
+  copy(includeCheckpoints: boolean = true): WrappedSecureTrie {
+    const db = this._mainDB.copy()
+    const trie = new WrappedSecureTrie(db._leveldb, this.root)
+    if (includeCheckpoints && this.isCheckpoint) {
+      trie._checkpoints = this._checkpoints.slice()
+      trie._scratch = this._scratch!.copy()
+      trie.db = trie._scratch
+    }
+    return trie
+  }
+
+  async checkRoot(root: Buffer, cb?: Callback<boolean | null>): Promise<boolean> {
+    return wrapPromise(super.checkRoot(root), cb).then((value) => !!value)
+  }
+
+  async commit(cb?: Callback<void>): Promise<void> {
+    return wrapEmptyPromise(super.commit(), cb)
+  }
+
+  async revert(cb?: Callback<void>): Promise<void> {
+    return wrapEmptyPromise(super.revert(), cb)
+  }
+}
+
+export { WrappedCheckpointTrie as CheckpointTrie }
+export { WrappedSecureTrie as SecureTrie }
