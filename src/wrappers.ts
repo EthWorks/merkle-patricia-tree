f5783001d071e4ed77a63ff81c67ada5697df186
Michał Sieczkowski <michal@ethworks.io>
2020-04-17 15:32:21 +0200

    Change get and commit wrappers order

--- src/wrappers.ts
+++ src/wrappers.ts
@@ -2,22 +2,6 @@ import { CheckpointTrie } from './checkpointTrie'
 import { SecureTrie } from './secure-original'
 
 class WrappedCheckpointTrie extends CheckpointTrie {
-  async commit(cb?: (err: any) => void): Promise<void> {
-    return super.commit().then(
-      (value) => {
-        cb?.(null)
-        return value
-      },
-      (err) => {
-        if (cb) {
-          cb(err)
-        } else {
-          throw err
-        }
-      },
-    )
-  }
-
   async get(key: Buffer, cb?: (err: any, value: Buffer | null) => void): Promise<Buffer | null> {
     return super.get(key).then(
       (value) => {
@@ -28,6 +12,22 @@ class WrappedCheckpointTrie extends CheckpointTrie {
         if (cb) {
           cb(err, null)
           return undefined as any
+        } else {
+          throw err
+        }
+      },
+    )
+  }
+
+  async commit(cb?: (err: any) => void): Promise<void> {
+    return super.commit().then(
+      (value) => {
+        cb?.(null)
+        return value
+      },
+      (err) => {
+        if (cb) {
+          cb(err)
         } else {
           throw err
         }
