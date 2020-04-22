c8787388264baea6dd5f17032d01ecbc0b2fdd3a
Michał Sieczkowski <michal@ethworks.io>
2020-04-22 18:33:33 +0200

    Create script and config for running browser tests with mocha

--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,56 @@
-module.exports = function (config) {
-  config.set({
-    frameworks: ['karma-typescript', 'tap'],
-    files: ['src/**/*.ts', 'test/**/*.ts'],
-    preprocessors: {
-      '**/*.ts': ['karma-typescript'],
+module.exports = function(config) {
+  const testRunner = process.env.TEST_ENV
+
+  switch (testRunner) {
+    case 'tape':
+      config.set(tapeConfig)
+      break
+    case 'mocha':
+      config.set(mochaConfig)
+      break
+  }
+}
+
+const commonConfig = {
+  frameworks: ['karma-typescript'],
+  files: ['src/**/*.ts', 'test/**/*.ts'],
+  preprocessors: {
+    '**/*.ts': ['karma-typescript'],
+  },
+  plugins: ['karma-typescript', 'karma-chrome-launcher', 'karma-firefox-launcher'],
+  karmaTypescriptConfig: {
+    tsconfig: './tsconfig.test.json',
+  },
+  colors: true,
+  browsers: ['FirefoxHeadless', 'ChromeHeadless'],
+  singleRun: true,
+  concurrency: Infinity,
+  // Fail after timeout
+  browserDisconnectTimeout: 100000,
+  browserNoActivityTimeout: 100000,
+}
+
+
+const tapeConfig = {
+  ...commonConfig,
+  frameworks: [...commonConfig.frameworks, 'tap'],
+  plugins: [...commonConfig.plugins, 'karma-tap'],
+  karmaTypescriptConfig: {
+    ...commonConfig.karmaTypescriptConfig,
+    bundlerOptions: {
+      entrypoints: /\.spec\.ts$/,
     },
-    plugins: ['karma-typescript', 'karma-tap', 'karma-chrome-launcher', 'karma-firefox-launcher'],
-    karmaTypescriptConfig: {
-      bundlerOptions: {
-        entrypoints: /\.spec\.ts$/,
-      },
-      tsconfig: "./tsconfig.test.json"
+  },
+}
+
+const mochaConfig = {
+  ...commonConfig,
+  frameworks: [...commonConfig.frameworks, 'mocha'],
+  plugins: [...commonConfig.plugins, 'karma-mocha'],
+  karmaTypescriptConfig: {
+    ...commonConfig.karmaTypescriptConfig,
+    bundlerOptions: {
+      entrypoints: /\.test\.ts$/,
     },
-    colors: true,
-    browsers: ['FirefoxHeadless', 'ChromeHeadless'],
-    singleRun: true,
-    concurrency: Infinity,
-    // Fail after timeout
-    browserDisconnectTimeout: 100000,
-    browserNoActivityTimeout: 100000,
-  })
+  },
 }
